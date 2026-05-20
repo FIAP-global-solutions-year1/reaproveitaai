d3a06c871fce577ebf34c4f7db771d74fa25a43b
phthedevx <pedro.h.santos20@ba.estudante.senai.br>
2026-05-20 16:16:34 -0300

    feat(js): personaliza mensagem de alerta com o nome do usuario

--- js/faleconosco.js
+++ js/faleconosco.js
@@ -1,6 +1,11 @@
 const formContato = document.getElementById('formContato');
 formContato.addEventListener('submit', function (event) {
     event.preventDefault();
-    alert("Mensagem enviada com sucesso! Nossa equipe retornará em breve.");
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const assunto = document.getElementById('assunto').value;
+    const mensagem = document.getElementById('mensagem').value;
+
+    alert(`Mensagem enviada com sucesso, ${nome}! Nossa equipe retornará em breve.`);
     formContato.reset();
 });
