d2acb7fac255d8ac9e9854c39265dae59a80e5ba
phthedevx <pedro.h.santos20@ba.estudante.senai.br>
2026-05-23 14:54:42 -0300

    feat(cnpj): adiciona mascara automatica com formatacao e validacao nativa

--- js/login.js
+++ js/login.js
@@ -40,7 +40,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     inputCnpj.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^0-9.\-\/]/g, '');
+        let value = e.target.value.replace(/\D/g, '');
+        if (value.length > 14) value = value.slice(0, 14);
+        value = value.replace(/^(\d{2})(\d)/, '$1.$2');
+        value = value.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
+        value = value.replace(/\.(\d{3})(\d)/, '.$1/$2');
+        value = value.replace(/(\d{4})(\d{1,2})$/, '$1-$2');
+        e.target.value = value;
     });
 
     gatilhoSeletor.addEventListener('click', () => {
