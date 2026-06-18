64a87d0bde08f8be3ffd65b357feab7269e868fd
Pedro Zigiotto <kyzampedro@gmail.com>
2026-06-18 18:20:18 -0300

    Validacao de nome e sobrenome
    adicionar validação de nome completo no formulário fale conosco

--- js/faleconosco.js
+++ js/faleconosco.js
@@ -2,6 +2,11 @@ const formContato = document.getElementById('formContato');
 formContato.addEventListener('submit', function (event) {
     event.preventDefault();
     const nome = document.getElementById('nome').value;
+    const partes = nome.trim().split(" ").filter(p => p !== "");
+    if (partes.length < 2) {
+        alert("Por favor, insira seu nome e sobrenome.");
+        return;
+    }
     const email = document.getElementById('email').value;
     const assunto = document.getElementById('assunto').value;
     const mensagem = document.getElementById('mensagem').value;
