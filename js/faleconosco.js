6119a34c021b53f97e99591a6d565fec33f3b5d6
eduardo-gotardi9 <du.gotardi@gmail.com>
2026-06-23 20:34:35 -0300

    feat: funcionalidade de verificaçao de nome e sobrenome contendo minimo de duas letras cada

--- js/faleconosco.js
+++ js/faleconosco.js
@@ -7,6 +7,10 @@ formContato.addEventListener('submit', function (event) {
         alert("Por favor, insira seu nome e sobrenome.");
         return;
     }
+    if (partes[0].length < 2 || partes[1].length < 2) {
+        alert("Tanto o nome quanto o sobrenome devem ter no mínimo duas letras.");
+        return;
+    }
     const email = document.getElementById('email').value;
     const assunto = document.getElementById('assunto').value;
     const mensagem = document.getElementById('mensagem').value;
